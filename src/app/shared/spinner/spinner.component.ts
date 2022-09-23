ce2d9dbd3d1a5258da5d620169a93f783487a938
Pablo <pablo.pita@tokiota.com>
2022-09-23 11:50:16 +0200

    reutilizable spinner

---
+++ src/app/shared/spinner/spinner.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spinner',
+  templateUrl: './spinner.component.html',
+  styleUrls: ['./spinner.component.scss'],
+})
+export class SpinnerComponent implements OnInit {
+  @Input() message!: string;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
