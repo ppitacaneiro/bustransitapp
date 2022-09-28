95937932c8a0feadb6e2ce6db9e75c7bf45e8b3f
Pablo <pablo.pita@tokiota.com>
2022-09-28 12:32:24 +0200

    import full calendar component

--- src/app/planning-trips/planning-trips.module.ts
+++ src/app/planning-trips/planning-trips.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { PlanningTripsComponent } from './planning-trips.component';
 import { PlanningTripsRoutingModule } from './planning-trips-routing.module';
 import { RouterModule } from '@angular/router';
+import { FullCalendarComponent } from './full-calendar/full-calendar.component';
 
 @NgModule({
-  declarations: [PlanningTripsComponent],
+  declarations: [PlanningTripsComponent, FullCalendarComponent],
   imports: [CommonModule, PlanningTripsRoutingModule, RouterModule],
 })
 export class PlanningTripsModule {}
