bcd56c5feca000ebb419dc56aebf79ddca5f6bed
Pablo <pablo.pita@tokiota.com>
2022-09-29 11:52:28 +0200

    reformat code

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { SpinnerComponent } from './spinner/spinner.component';
 import { HeaderComponent } from './header/header.component';
@@ -7,8 +8,16 @@ import { SimpleToastComponent } from './simple-toast/simple-toast.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [HeaderComponent, SpinnerComponent, SimpleToastComponent],
-  imports: [CommonModule, NgbModule, RouterModule],
-  exports: [HeaderComponent, SpinnerComponent, SimpleToastComponent],
+  declarations: [
+    HeaderComponent,
+    SpinnerComponent,
+    SimpleToastComponent,
+  ],
+  imports: [CommonModule, NgbModule, RouterModule, FormsModule],
+  exports: [
+    HeaderComponent,
+    SpinnerComponent,
+    SimpleToastComponent,
+  ],
 })
 export class SharedModule {}
