e9f6cc8d005334c3aab4882563f87461f26c8483
Pablo <pablo.pita@tokiota.com>
2022-09-29 11:50:09 +0200

    imported some components and modules

--- src/app/planning-trips/planning-trips.module.ts
+++ src/app/planning-trips/planning-trips.module.ts
@@ -1,12 +1,27 @@
+import { SharedModule } from './../shared/shared.module';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlanningTripsComponent } from './planning-trips.component';
 import { PlanningTripsRoutingModule } from './planning-trips-routing.module';
 import { RouterModule } from '@angular/router';
 import { FullCalendarComponent } from './full-calendar/full-calendar.component';
+import { EditFormComponent } from './edit-form/edit-form.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [PlanningTripsComponent, FullCalendarComponent],
-  imports: [CommonModule, PlanningTripsRoutingModule, RouterModule],
+  declarations: [
+    PlanningTripsComponent,
+    FullCalendarComponent,
+    EditFormComponent,
+  ],
+  imports: [
+    CommonModule,
+    PlanningTripsRoutingModule,
+    RouterModule,
+    NgbModule,
+    FormsModule,
+    SharedModule,
+  ],
 })
 export class PlanningTripsModule {}
